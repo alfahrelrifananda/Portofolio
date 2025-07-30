0ecff112e5cb325b974883341f8cf4ade85327e4
alfahrelrifananda <pahrel1234@gmail.com>
2025-07-30 19:54:13 +0700

    Now

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,18 @@
 import Style from "../assets/Nav.module.css"
 
 export default function Nav() {
+
+    function menuToggle() {
+        const navbarUl = document.getElementById("navbar-ul")
+        navbarUl?.classList.toggle('show')
+    }
+    
     return (
         <>
         <nav className={Style.Navbar}>
             <div>Alfahrel Rifananda</div>
-            <ul>
+            <div className={Style.menu} onClick={menuToggle}>Menu</div>
+            <ul id="navbar-ul">
                 <li><a href="">About</a></li>
                 <li><a href="">Expertise</a></li>
                 <li><a href="">Experience</a></li>
