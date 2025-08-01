3e951a4b4ba872526bc66678bef09b38a2b93de1
alfahrelrifananda <pahrel1234@gmail.com>
2025-08-01 10:07:44 +0700

    commit

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -20,42 +20,56 @@ export default function Experience() {
                     <th>Technology</th>
                     <th>Language</th>
                     <th>Position</th>
+                    <th>Company</th>
                 </tr>
                 <tr>
                     <td>SMPN 8 Solo</td>
                     <td>Wordpress and Figma</td>
                     <td>PHP</td>
                     <td>Prototype and Development</td>
+                    <td>Digimizu Digital Management</td>
                 </tr>
                 <tr>
                     <td>SMAN 8 Solo</td>
                     <td>Wordpress and Figma</td>
                     <td>PHP</td>
                     <td>Prototype and Development</td>
+                    <td>Digimizu Digital Management</td>
                 </tr>
                  <tr>
                     <td>Andala Library</td>
                     <td>Wordpress and Figma</td>
                     <td>PHP</td>
                     <td>Prototype and Development</td>
+                    <td>Digimizu Digital Management</td>
                 </tr>
                 <tr>
                     <td>Pos Psikologi</td>
                     <td>Vanilla HTML5, CSS3, and JavaScript</td>
                     <td>JavaScript</td>
                     <td>Full Stack</td>
+                    <td>Freelance</td>
                 </tr>
                 <tr>
                     <td>Chameleon</td>
                     <td>Flutter</td>
                     <td>Dart</td>
-                    <td>Full Stack</td>
+                    <td>Developer</td>
+                    <td>Freelance</td>
                 </tr>
                 <tr>
                     <td>Rel Weather</td>
                     <td>MDC Android</td>
                     <td>Java</td>
-                    <td>Full Stack</td>
+                    <td>Developer</td>
+                    <td>Freelance</td>
+                </tr>
+                <tr>
+                    <td>Rel Wallpaper</td>
+                    <td>MDC Android</td>
+                    <td>Java</td>
+                    <td>Developer</td>
+                    <td>Freelance</td>
                 </tr>
                </table>
               </div>
